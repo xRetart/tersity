bbda1caa5a1bb26fca3b2354dbedd585afdc47d7
xRetart <richard.network96@gmail.com>
2021-03-29 12:26:31 +0200

    automate installation process with "install.sh" script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+
+function build_install
+{
+    (cd $1 && make && make install)
+}
+
+# build and install compiler
+build_install compiler
+
+# build and install stdandard library
+build_install stdlib
